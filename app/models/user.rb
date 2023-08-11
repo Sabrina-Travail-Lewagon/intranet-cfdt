ca89adf43db305e34b0cde02877f01e0168f5d7e
Sabrina MOUEDINE <mouedines@cfdt-services.fr>
2023-08-11 14:43:42 +0200

    ajout validations users : first_name, last_name et role

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,21 @@
 class User < ApplicationRecord
+  ROLES = ['admin', 'user', 'rh', 'cse', 'redacteur'] # On stocke les rôles possibles
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  # validation inclusion, garantit que le role spécifié est une des valeurs autorisés
+  validates :role, inclusion: { in: ROLES }
+
+  # before_validation définira automatiquement le rôle par défaut sur user avant de valider l'utilisateur.
+  before_validation :set_default_role
+
+  private
+
+  # On va mettre le role user par défaut
+  def set_default_role
+    self.role ||= 'user'
+  end
 end
