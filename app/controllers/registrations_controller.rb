fe18631e0294fb78693348b1f85fae256cdaa7d0
Sabrina MOUEDINE <mouedines@cfdt-services.fr>
2023-08-11 17:02:38 +0200

    ajout bootstrap js, et modif navbar pour aff photo user

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,2 +1,19 @@
-class RegistrationsController < ApplicationController
+class RegistrationsController < Devise::RegistrationsController
+  before_action :configure_permitted_parameters
+  # GET /users/sign_up
+  def new
+    super
+  end
+
+  # POST /users
+  def create
+    # Votre code personnalisé ici
+    super
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :photo])
+  end
 end
