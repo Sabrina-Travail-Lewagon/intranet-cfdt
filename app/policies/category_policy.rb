450878972f1b6beb8e42499f72df233a60a39e82
Sabrina MOUEDINE <mouedines@cfdt-services.fr>
2023-08-16 17:27:33 +0200

    ajout de la policy pour categories et paramétrage

---
+++ app/policies/category_policy.rb
@@ -0,0 +1,27 @@
+class CategoryPolicy < ApplicationPolicy
+  class Scope < Scope
+    # NOTE: Be explicit about which records you allow access to!
+    # def resolve
+    #   scope.all
+    # end
+    def index?
+      true
+    end
+
+    def show?
+      true
+    end
+
+    def create?
+      user.admin? || user.rh? # Autorise admin ou rh à créer une catégorie
+    end
+
+    def update?
+      user.admin? || user.rh? # Autorise admin ou rh à mettre à jour une catégorie
+    end
+
+    def destroy?
+      user.admin? || user.rh? # Autorise admin ou rh à supprimer une catégorie
+    end
+  end
+end
