e04ec386cf32f8d3474368f2a580bcc2733c62aa
Sabrina MOUEDINE <mouedines@cfdt-services.fr>
2023-08-16 17:23:25 +0200

    création des methodes dans controller categories, creation vues pour categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,29 @@
 class CategoriesController < ApplicationController
+  def index
+    @categories = Category.all.order('created_at DESC')
+  end
+
+  def new
+    @user = current_user
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      redirect_to  categories_path(@category), alert: 'Categorie créée!'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @category = Category.find(params[:id])
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:nom)
+  end
 end
