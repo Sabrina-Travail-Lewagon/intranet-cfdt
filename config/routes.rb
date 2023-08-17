f8d2476a0d559b1178a59a644bb4c93dc304bf56
Sabrina MOUEDINE <mouedines@cfdt-services.fr>
2023-08-17 12:59:03 +0200

    ajout des routes pour les articles et les catégories (avec member)

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
   # devise_for :users, controllers: { registrations: 'registrations'}
   root to: "pages#home"
   resources :articles
-  resources :categories
+  resources :categories do
+    member do
+      get 'articles', to: 'categories#category_articles'
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
