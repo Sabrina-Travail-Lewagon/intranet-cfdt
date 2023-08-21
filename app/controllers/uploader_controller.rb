5f50242b0baf64e00d63493ef6daadd2b7b66590
Sabrina MOUEDINE <mouedines@cfdt-services.fr>
2023-08-21 15:33:05 +0200

    modif controller uploader

--- app/controllers/uploader_controller.rb
+++ app/controllers/uploader_controller.rb
@@ -1,4 +1,9 @@
 class UploaderController < ApplicationController
   def image
+    image_data = params[:file]
+    # Utilisez le SDK Cloudinary pour envoyer l'image à Cloudinary
+    cloudinary_response = Cloudinary::Uploader.upload(image_data)
+    # Renvoyez une réponse JSON avec l'URL de l'image envoyée à Cloudinary
+    render json: { location: cloudinary_response["secure_url"] }, status: :created
   end
 end
