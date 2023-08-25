9276a9187af424d505c6d273fb8d2166c2c2d4e2
Sabrina MOUEDINE <mouedines@cfdt-services.fr>
2023-08-25 16:51:27 +0200

    modif application.html.erb modif tiny.init ajout image_tools

--- app/controllers/uploader_controller.rb
+++ app/controllers/uploader_controller.rb
@@ -8,7 +8,8 @@ class UploaderController < ApplicationController
     @user = current_user # Pour que le header puisse s'afficher avec le login
     image_data = params[:file]
     # Utilisez le SDK Cloudinary pour envoyer l'image à Cloudinary
-    cloudinary_response = Cloudinary::Uploader.upload(image_data)
+    # cloudinary_response = Cloudinary::Uploader.upload(image_data)
+    cloudinary_response = Cloudinary::Uploader.upload(image_data, use_filename: true, unique_filename: true)
     # Renvoyez une réponse JSON avec l'URL de l'image envoyée à Cloudinary
     render json: { location: cloudinary_response["secure_url"] }, status: :created
   end
